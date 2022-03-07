7f53f693491cdffeec8a493e2cf35ea1026adedf
sandal <ru-sandal@yandex.ru>
2022-03-08 00:07:11 +0300

    Modify: delete dublicate and added switch

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -18,58 +18,52 @@ public class Parser {
 
     private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
 
-    private static String getDateFromString(String stringDate) throws Exception{
+    private static String getDateFromString(String stringDate) throws Exception {
         Matcher matcher = pattern.matcher(stringDate);
-        if(matcher.find()){
+        if (matcher.find()) {
             return matcher.group();
         }
         throw new Exception("Can't extract date from string");
     }
 
-    private static int printFourValues(Elements values, int index){
-
-        int iterationCount=4;
-        if(index==0){
-            if(values.size()==19){
-                iterationCount=3;
-            } else if (values.size()==18){
-                iterationCount=2;
-            } else if (values.size()==17){
-                iterationCount=1;
-            }
-            for(int i = 0; i<iterationCount; i++){
-                Element valueLine=values.get(index + i);
-                for(Element td:valueLine.select("td")){
-                    System.out.print(td.text() + "  ");
-                }
-                System.out.println();
-            }
-        } else {
-            for(int i = 0; i<iterationCount; i++){
-                Element valueLine=values.get(index + i);
-                for(Element td:valueLine.select("td")){
-                    System.out.print(td.text() + "  ");
-                }
-                System.out.println();
+    private static int printFourValues(Elements values, int index) {
+        int iterationCount = 4;
+        if (index == 0) {
+            switch (values.size()){
+                case 19:
+                    iterationCount = 3;
+                    break;
+                case 18:
+                    iterationCount = 2;
+                    break;
+                case 17:
+                    iterationCount = 1;
+                    break;
             }
         }
-
+        for (int i = 0; i < iterationCount; i++) {
+            Element valueLine = values.get(index + i);
+            for (Element td : valueLine.select("td")) {
+                System.out.print(td.text() + "  ");
+            }
+            System.out.println();
+        }
         return iterationCount;
 
     }
 
     public static void main(String[] args) throws Exception {
-        Document page=getPage();
+        Document page = getPage();
         Element tableWeather = page.select("table[class=wt]").first();
         Elements names = tableWeather.select("tr[class=wth]");
         Elements values = tableWeather.select("tr[valign=top]");
         int index = 0;
-        for(Element name:names){
-            String dateString=name.select("th[id=dt]").text();
+        for (Element name : names) {
+            String dateString = name.select("th[id=dt]").text();
             String date = getDateFromString(dateString);
-            System.out.println(date+" Явление  Температура Давление Влажность Ветер");
+            System.out.println(date + " Явление  Температура Давление Влажность Ветер");
             int iterationCount = printFourValues(values, index);
-            index = iterationCount+index;
+            index = iterationCount + index;
         }
     }
 }
